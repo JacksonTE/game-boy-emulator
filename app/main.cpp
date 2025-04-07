ce17e58da02e5ac898a325246c957d1b1af6341e
Jackson Enright <jacksonenright2001@gmail.com>
2025-04-07 14:56:38 -0400

    add json tests and blargg tests as git submodule

--- app/main.cpp
+++ app/main.cpp
@@ -22,7 +22,8 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    std::filesystem::path test_rom_path = std::filesystem::path(PROJECT_ROOT) / "bootrom" / "cpu_instrs" / "individual" / "02-interrupts.gb";
+    std::filesystem::path test_rom_path = std::filesystem::path(PROJECT_ROOT) / 
+        "tests" / "data" / "blargg-tests" / "gb-test-roms" / "cpu_instrs" / "individual" / "06-ld r,r.gb";
     game_boy_emulator.try_load_file_to_memory(0x0000, GameBoy::COLLECTIVE_ROM_BANK_SIZE, test_rom_path, false);
 
     if (bootrom_path.empty()) {
