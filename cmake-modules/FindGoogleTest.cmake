20f132ebc7e34f7d2e26735baaf3d76489ff757b
Jackson Enright <jacksonenright2001@gmail.com>
2025-11-02 13:22:18 -0500

    Add new cmake-modules folder missed in previous commit

---
+++ cmake-modules/FindGoogleTest.cmake
@@ -0,0 +1,11 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    GoogleTest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v1.17.0
+    GIT_SHALLOW TRUE)
+
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(GoogleTest)
---
+++ cmake-modules/FindImGui.cmake
@@ -0,0 +1,24 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    ImGui
+    GIT_REPOSITORY https://github.com/ocornut/imgui.git
+    GIT_TAG v1.91.9b
+    GIT_SHALLOW TRUE)
+
+FetchContent_MakeAvailable(ImGui)
+
+add_library(ImGui STATIC
+    ${imgui_SOURCE_DIR}/imgui.cpp
+    ${imgui_SOURCE_DIR}/imgui_draw.cpp
+    ${imgui_SOURCE_DIR}/imgui_tables.cpp
+    ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+    ${imgui_SOURCE_DIR}/backends/imgui_impl_sdl3.cpp
+    ${imgui_SOURCE_DIR}/backends/imgui_impl_sdlrenderer3.cpp)
+
+target_include_directories(ImGui PUBLIC
+    ${imgui_SOURCE_DIR}
+    ${imgui_SOURCE_DIR}/backends)
+
+target_link_libraries(ImGui PUBLIC
+    SDL3::SDL3)
