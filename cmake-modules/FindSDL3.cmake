52ef859ccd5a58e84cf76dd1669802f6ae351075
Jackson Enright <jacksonenright2001@gmail.com>
2025-11-09 22:41:01 -0500

    Remove font files from repo, switch to pulling them from github and converting the ttf to byte array in cmake

--- cmake-modules/FindSDL3.cmake
+++ cmake-modules/FindSDL3.cmake
@@ -6,9 +6,9 @@ FetchContent_Declare(
     GIT_TAG release-3.2.12
     GIT_SHALLOW TRUE)
 
+FetchContent_MakeAvailable(SDL3)
+
 set(SDL_TESTS OFF CACHE BOOL "" FORCE)
 set(SDL_EXAMPLES OFF CACHE BOOL "" FORCE)
 set(SDL_SHARED OFF CACHE BOOL "" FORCE)
 set(SDL_STATIC ON CACHE BOOL "" FORCE)
-
-FetchContent_MakeAvailable(SDL3)
