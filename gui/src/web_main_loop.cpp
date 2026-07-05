8bbb131bd081ddfb750ba2846cace2d2080ffba3
Jackson Enright <jacksonenright2001@gmail.com>
2026-07-05 19:23:17 -0400

    Revert "to revert: add debug logging to identify cause of blurry text only in deployed version"
    This reverts commit a9cc719de21640aa5b3311178d4847d92b7e9da6.

--- gui/src/web_main_loop.cpp
+++ gui/src/web_main_loop.cpp
@@ -4,7 +4,6 @@
 
 #include <emscripten.h>
 #include <emscripten/html5.h>
-#include <iostream>
 
 #include "display_utilities.h"
 #include "input_events.h"
@@ -21,11 +20,6 @@ static void sync_emscripten_window_to_viewport(
     {
         SDL_SetWindowSize(state->sdl_window, viewport_w, viewport_h);
     }
-
-    std::cout << "[web_viewport] viewport_w=" << viewport_w
-              << " viewport_h=" << viewport_h
-              << " previous_sdl_w=" << sdl_w
-              << " previous_sdl_h=" << sdl_h << "\n";
 
     update_imgui_scale_by_resolution(state->sdl_window);
     render_frame(*state->render_context);
