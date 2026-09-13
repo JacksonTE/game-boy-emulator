c6766c0482615e808fc053ab9a597e9ec8d8ecb1
Jackson Enright <jacksonenright2001@gmail.com>
2026-09-13 12:19:33 -0400

    Update NFDe tag being pulled to latest version of master instead.

--- cmake/FindNativeFileDialogExtended.cmake
+++ cmake/FindNativeFileDialogExtended.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     NativeFileDialogExtended
     GIT_REPOSITORY https://github.com/btzy/nativefiledialog-extended.git
-    GIT_TAG v1.3.0
+    GIT_TAG master
     GIT_SHALLOW TRUE)
 
 FetchContent_MakeAvailable(NativeFileDialogExtended)
