0ae84d2dd5467037fb2d0f7f04037531dbbe6945
Jackson Enright <jacksonenright2001@gmail.com>
2026-09-13 01:02:09 -0400

    Update FindNativeFileDialogExtended.cmake to pull from a known release tag. The manual commit hashes being used have been occasionally becoming unavailable.

--- cmake/FindNativeFileDialogExtended.cmake
+++ cmake/FindNativeFileDialogExtended.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     NativeFileDialogExtended
     GIT_REPOSITORY https://github.com/btzy/nativefiledialog-extended.git
-    GIT_TAG 3cd252a8f7ca32419b1ca235c2990ba6a0ecba7c
+    GIT_TAG v1.3.0
     GIT_SHALLOW TRUE)
 
 FetchContent_MakeAvailable(NativeFileDialogExtended)
